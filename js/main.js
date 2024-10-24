5b32fa0c4be6ab6e5d9ed73c1820424630abe571
versace 🦍 <bardakov192@gmail.com>
2024-10-25 01:07:52 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new Swiper(".top__swiper", {
+        // Optional parameters
+        effect: 'fade',
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    new Swiper(".about__slider", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        freeMode: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+    item.addEventListener('click', () => {
+        item.parentNode.classList.toggle('accordeon__item--active');
+    })
+});
